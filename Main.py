c435a50954a8ded8ac96214c5d47a6c86ff39b63
EdisonNunes <32773694+EdisonNunes@users.noreply.github.com>
2023-12-21 13:38:06 -0300

    Add files via upload
    Modificado para buscar dados dos últimos 12 meses

--- Main.py
+++ Main.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import locale
 
+# ####  streamlit run Main.py
 
 #from pyecharts import options as opts
 #from pyecharts.charts import Line
@@ -16,48 +17,64 @@ st.set_page_config(layout='centered')
 @st.cache_data  # Coloca os dados no Cache
 def gerar_df():
     df = pd.read_excel('ConsumoCond.xlsx')
+    # df = pd.read_excel('ConsumoCondTeste.xlsx')
     df['ANO_NUM'] = df['Data'].dt.year
     df['ANO_NUM'] = df['ANO_NUM'].astype(str)
     df['MES_NUM'] = df['Data'].dt.month
-    df['MES_STR'] = df['Data'].dt.strftime('%b')
+    df['MES_STR'] = df['Data'].dt.strftime('%b') + '/' + df['Data'].dt.strftime('%y')
     df['Consumo'] = df['Consumo'].fillna(0.0).astype(int)
     return df
 
 locale.setlocale(locale.LC_ALL, 'pt_BR.utf8')
 df = gerar_df()
+df_last = df.sort_values(['Data'], ascending=[False])
 
 st.title('Conjunto Residencial Jardim Sabará')
 
 col1, col2 = st.columns(2, gap= 'small')
-with col1:
-    fAno = st.selectbox('ANO',
-                        options=df['ANO_NUM'].unique(),
-                        index= 8)
 
-    dadosAno = df.loc[(
-        df['Data'] == fAno
-    )]
+lista_anos = df['ANO_NUM'].unique()
+lista_anos_invertida = lista_anos[::-1]
+opcao =[]
+opcao.append('Últimos 12 meses')
+for i in range(0, len(lista_anos_invertida)):
+    opcao.append(lista_anos_invertida[i])
+
+with col1:
+    fAno = st.selectbox('ANO/PERÍODO',
+                        # options=df['ANO_NUM'].unique(),
+                        options=opcao,
+                        index= 0)
+    dadosAno = df.loc[(df['Data'] == fAno)]
+
 with col2:
     fEmpresa = st.selectbox('EMPRESA',
                             options=df['Empresa'].unique()
                             )
+    if fEmpresa == 'Sabesp':
+        Unidade = 'm³'
+    else:
+        Unidade = 'kw'
 
-dadosUsuario = df.loc[(
-    df['Empresa'] == fEmpresa) & (df['ANO_NUM'] == fAno)
-]
+    if fAno == 'Últimos 12 meses':
+        dadosUsuario = df_last.loc[(df_last['Empresa'] == fEmpresa)][:12]
+        dadosUsuario = dadosUsuario.sort_values(['Data'])
+        st.header('Consumo nos últimos 12 meses em ' + Unidade)
+        Texto = 'Consumo em 12 meses'
+    else:
+        dadosUsuario = df.loc[(df['Empresa'] == fEmpresa) & (df['ANO_NUM'] == fAno)]
+        st.header('Consumo no ano de ' + fAno + ' em ' + Unidade)
+        Texto = 'Consumo em ' + fAno
 
 
-updateDatas = dadosUsuario['Data'].dt.strftime('%Y/%b')
-dadosUsuario['Data']= updateDatas[0:]
 
-updateAno = dadosAno['Data'].dt.strftime('%Y')
-dadosAno = updateAno[0:]
+#updateDatas = dadosUsuario['Data'].dt.strftime('%Y/%b')
+#dadosUsuario['Data']= updateDatas[0:]
 
-if fEmpresa == 'Sabesp':
-    Unidade = 'm³'
-else:
-    Unidade = 'kw'
-st.header('Consumo no ano de '+ fAno + ' em ' + Unidade)
+#updateAno = dadosAno['Data'].dt.strftime('%Y')
+#dadosAno = updateAno[0:]
+
+
 
 fig, ax = plt.subplots()
 
@@ -73,10 +90,11 @@ for i in y:
     Media.append(z)
 
 ########## Imprime Gráfico
-Texto = 'Consumo em ' + fAno
+plt.rc('xtick', labelsize=8)
+
 plt.plot(x , y, label=Texto, marker='*')
 plt.plot(x , Media, label= 'Consumo médio '+ str(z) + Unidade, color='r')
-plt.xlabel('Meses do Ano')
+# plt.xlabel('Meses do Ano')
 plt.ylabel('Valor consumido em ' + Unidade)
 
 plt.legend()
